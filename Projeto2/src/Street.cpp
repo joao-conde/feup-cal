e78a5253aaca672a072345aca0e4cdb5071f3547
julietafrade97 <up201506530@fe.up.pt>
2017-05-09 15:48:24 +0100

    projeto 2 + fix da janela

---
+++ Projeto2/src/Street.cpp
@@ -0,0 +1,38 @@
+#include "Street.h"
+
+Street::Street() {
+}
+
+Street::Street(int id, string name, vector<Vertex<Node>*> vertexs, int way) {
+	this->id = id;
+	this->name = name;
+	this->vertexs = vertexs;
+
+	if (way == 1)
+		this->twoWays = true;
+	else
+		this->twoWays = false;
+}
+
+Street::~Street() {
+}
+
+int Street::getID() {
+	return this->id;
+}
+
+string Street::getName() {
+	return this->name;
+}
+
+vector<Vertex<Node>*> Street::getVertexs() {
+	return this->vertexs;
+}
+
+bool Street::isTwoWays() {
+	return this->twoWays;
+}
+
+string Street::getLocation() {
+	return this->location;
+}
